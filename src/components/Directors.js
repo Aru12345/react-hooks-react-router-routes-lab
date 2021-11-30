09d83fb262aa315585f9729bca64302be90c603a
Arundhati <arundhatiraicar12@gmail.com>
2021-11-30 23:41:29 +0530

    First

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,33 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    <h2>Name:Scott Derrickson</h2>
+    <p>Movies:</p>
+    <ul>
+      <li>Doctor Strange</li>
+      <li>Sinister</li>
+      <li>The Exorcism of Emily Rose</li>
+    </ul>
+
+    <h2>Name:Mike Mitchell</h2>
+    <p>Movies:</p>
+    <ul>
+      <li>Trolls</li>
+      <li>Alvin and the Chipmunks: Chipwrecked</li>
+      <li>Sky High</li>
+    </ul>
+
+    <h2>Name:Edward Zwick</h2>
+    <p>Movies:</p>
+    <ul>
+      <li>Jack Reacher</li>
+      <li>Never Go Back</li>
+      <li>Blood Diamon</li>
+      <li>The Siege</li>
+    </ul>
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,16 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesList=movies.map((movie)=>(
+    <div>{movie.title}
+    {movie.time}
+    <ul><li>{movie.genre}</li></ul>
+    </div>
+  ))
+  return <div>{<h1>Movies Page</h1>,moviesList
+    
+    
+    /*{code here}*/}</div>;
 }
 
 export default Movies;
